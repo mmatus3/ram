2bffb5a2eaad17956915d260fdbb0a3dc04fdeb3
Noah Neugebauer <noah.t.neugebauer@pwc.com>
2018-06-12 14:23:37 +0200

    Date time

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import datetime
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return datetime.datetime.now()
 
 if __name__ == '__main__':
   app.run()
